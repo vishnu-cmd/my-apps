2cfdf92bc9a8c2c2796b13dfc33991aeeeec8595
vishnu_py <vishnuthecrazy1@gmail.com>
2023-04-21 00:26:03 +0530

    product component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   {
     path:'login',
     component: LoginComponent
+  },
+  {
+    path:'products',
+    component: ProductComponent
   }
 ];
 
